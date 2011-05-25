d5e0da72a28b9c2731803b84ecbd02bd0c8d8080
I <afa@afalone.local>
2011-05-25 12:29:09 +0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Urm::Application.config.secret_token = 'b66df24226a7681796fdcbc03c2de6189b641347b2564236db08f1c5e9e88d295cbdf5569fd163bbd6fcd0ef9ae0ca7e255a8d09e7bca1e7401e1d50b35ce41f'
