d00d29415813f0ae86fe0d1525c14635bbb7b2f4
afa <afa@afalone.local>
2011-06-28 18:55:40 +0400

    login init

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,18 @@
 class SessionsController < ApplicationController
   def new
+   
   end
 
   def create
+   if @current_user = User.authenticate(params["session"]["username"], params["session"]["password"])
+    session[:user] = @current_user.id
+   end
+   redirect_to root_path
   end
 
   def destroy
+   session[:user] = nil
+   redirect_to root_path
   end
 
 end
