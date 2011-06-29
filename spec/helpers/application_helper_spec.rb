3583eb10f21c088304b7987ebcdc3586a5d174b2
afa <afa@afalone.local>
2011-06-29 12:05:45 +0400

    helper specs

---
+++ spec/helpers/application_helper_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+# Specs in this file have access to a helper object that includes
+# the MainHelper. For example:
+#
+# describe MainHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       helper.concat_strings("this","that").should == "this that"
+#     end
+#   end
+# end
+describe ApplicationHelper do
+ describe "current_user" do
+  context "when user valid" do
+   before do
+    User.any_instance.stub(:valid?).and_return(true)
+    @user = Factory(:user, :ext_hash => 'aaa')
+    session[:user] = @user.id
+   end
+   it "should locate logged user" do
+    current_user.should_not be_nil
+    current_user.should == @user
+   end
+  end
+ end
+ describe "logged_in?" do
+ end
+
+end
+
