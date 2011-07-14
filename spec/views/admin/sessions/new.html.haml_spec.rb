ec291e8458eb8572857134b933e0b1feae21120a
afa <afa@afalone.local>
2011-07-14 17:30:53 +0400

    spec

--- spec/views/admin/sessions/new.html.haml_spec.rb
+++ spec/views/admin/sessions/new.html.haml_spec.rb
@@ -4,18 +4,6 @@ describe "admin/sessions/new.html.haml" do
  before do
   render
  end
- it { response.should have_xpath("//form[@action='/admin/sessions']//input[@type='text' and @name='session[name]']") }
- it { response.should have_xpath("//form[@action='/admin/sessions']//input[@type='password' and @name='session[password]']") }
+ it { rendered.should have_xpath("//form[@action='/admin/sessions']//input[@type='text' and @name='session[name]']") }
+ it { rendered.should have_xpath("//form[@action='/admin/sessions']//input[@type='password' and @name='session[password]']") }
 end
-
-#%h2 Sign in
-#- form_for :session, :url => admin_sessions_path do |f|
-# .field
-#  = f.label :name
-#  = f.text_field :name
-# .field
-#  = f.label :password
-#  = f.password_field :password
-# .field
-#  = f.submit "sign in"
-
--- spec/views/admin/users/index.html.haml_spec.rb
+++ spec/views/admin/users/index.html.haml_spec.rb
@@ -4,5 +4,5 @@ describe "admin/users/index.html.haml" do
  before do
   render
  end
- it { response.should render_template("admin/users/_list") }
+ it { rendered.should render_template("admin/users/_list") }
 end
