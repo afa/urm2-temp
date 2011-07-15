f5824a2c95667ba57c1816c398d9028995ce034d
afa <afa@afalone.local>
2011-07-15 16:52:01 +0400

    specs/main

--- spec/views/admin/main/index.html.haml_spec.rb
+++ spec/views/admin/main/index.html.haml_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
-describe "main/index.html.haml" do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "admin/main/index.html.haml" do
+ before do
+  User.any_instance.stub(:valid?).and_return(true)
+  User.any_instance.stub(:unique_hash).and_return(true)
+  Account.any_instance.stub(:validates_uniqueness_of).and_return(true)
+  @users = FactoryGirl.create_list(:user, 5, :ext_hash => rand(1000).to_s)
+  session[:manager] = FactoryGirl.create(:manager).id
+  assign(:users, @users)
+  render
+ end
+ it { rendered.should have_xpath("//a[@href='/admin/managers']") }
+ it "should list users" do
+  @users.each{| u | rendered.should have_xpath("//a[@href='/admin/users/#{u.id}/passwords/edit']", :text => u.username) }
+ end
 end
