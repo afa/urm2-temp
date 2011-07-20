bad97a6cee4b9bf32a929be6fffa4558c5e5fa41
afa <afa@afalone.local>
2011-07-20 13:54:01 +0400

    sp

--- spec/views/admin/managers/edit.html.haml_spec.rb
+++ spec/views/admin/managers/edit.html.haml_spec.rb
@@ -2,8 +2,10 @@ require 'spec_helper'
 
 describe "admin/managers/edit.html.haml" do
  before do
+  @manager = FactoryGirl.create(:manager)
+  assign(:manager, @manager)
   render
  end
- it { rendered.should have_xpath("//form[@action='/admin/managers']//input[@type='text' and @name='manager[name]']") }
- it { rendered.should have_xpath("//form[@action='/admin/managers']//input[@type='password' and @name='manager[password]']") }
+ it { rendered.should have_xpath("//form[@action='/admin/managers/#{@manager.id}']//input[@type='text' and @name='manager[name]']") }
+ it { rendered.should have_xpath("//form[@action='/admin/managers/#{@manager.id}']//input[@type='password' and @name='manager[password]']") }
 end
