b429eff4aa361418c5e673cf1194ffc464505b68
afa <afa@afalone.local>
2011-07-20 15:20:34 +0400

    spec

--- spec/views/admin/managers/show.html.haml_spec.rb
+++ spec/views/admin/managers/show.html.haml_spec.rb
@@ -1,5 +1,37 @@
 require 'spec_helper'
 
-describe "managers/show.html.haml" do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "admin/managers/show.html.haml" do
+ before do
+  @login = FactoryGirl.create(:manager)
+  @manager = FactoryGirl.create(:manager)
+  assign(:manager, @manager)
+ end
+ context "when login super" do
+  before do
+   @superlogin = FactoryGirl.create(:manager, :super => true)
+   assign(:current_user, @superlogin)
+   render
+  end
+  it "should render super" do
+   rendered.should have_content("Супер")
+  end
+  it "should render kink to new manager" do
+   rendered.should have_xpath("//a[@href='#{new_admin_manager_path}']")
+  end
+ end
+ context "when logged" do
+  before do
+   assign(:current_user, @login)
+   render
+  end
+  it "should render name" do
+   rendered.should have_content(@manager.name)
+  end
+  it "should render link to edit" do
+   rendered.should have_xpath("//a[@href='#{edit_admin_manager_path(@manager)}']")
+  end
+  it "should render link to users list" do
+   rendered.should have_xpath("//a[@href='#{users_path}']")
+  end
+ end
 end
