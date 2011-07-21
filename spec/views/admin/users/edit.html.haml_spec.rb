9df6f65016bf341f3fcb94be7aa1a12fff93c45e
afa <afa@afalone.local>
2011-07-21 17:00:40 +0400

    views

--- spec/views/admin/users/edit.html.haml_spec.rb
+++ spec/views/admin/users/edit.html.haml_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe "admin/users/edit.html.haml" do
-  pending "add some examples to (or delete) #{__FILE__}"
+ before do
+  Axapta.stub!(:user_info).and_return({})
+  @user = FactoryGirl.create(:user)
+  assign(:user, @user)
+  render
+ end
+ specify { rendered.should have_xpath("//form[@action='#{admin_user_path(@user)}']") }
 end
