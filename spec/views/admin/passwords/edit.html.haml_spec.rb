47d09fb2dcafcda25da86600b64402245b53c89f
afa <afa@afalone.local>
2011-07-22 11:40:30 +0400

    spec

--- spec/views/admin/passwords/edit.html.haml_spec.rb
+++ spec/views/admin/passwords/edit.html.haml_spec.rb
@@ -1,5 +1,12 @@
 require 'spec_helper'
 
-describe "passwords/edit.html.haml" do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "admin/passwords/edit.html.haml" do
+ before do
+  Axapta.stub!(:user_info).and_return({})
+  @user = FactoryGirl.create(:user)
+  assign(:user, @user)
+  render
+ end
+ it { rendered.should have_xpath("//form[@action='#{admin_user_passwords_path(@user)}']") }
+ it { rendered.should have_xpath("//form//input[@type='checkbox' and @name='password[confirmed]']") }
 end
