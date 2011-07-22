4546a86b07b2248e1cb2bf0a1d1c6ef52594e1d3
afa <afa@afalone.local>
2011-07-22 10:55:28 +0400

    spec

--- spec/views/admin/passwords/index.html.haml_spec.rb
+++ spec/views/admin/passwords/index.html.haml_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
-describe "passwords/index.html.haml" do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "admin/passwords/index.html.haml" do
+ before do
+  Axapta.stub!(:user_info).and_return({})
+  @user = FactoryGirl.create(:user)
+  assign(:user, @user)
+  render
+ end
+
+ it { rendered.should have_content(@user.username) }
+ it { rendered.should have_content(@user.email) }
+ it { rendered.should have_content(@user.ext_hash) }
+ it { rendered.should have_xpath("//a[@href='#{edit_admin_user_passwords_path(@user)}']") }
 end
