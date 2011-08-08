31b5059b662c1a047e9387a4079cd9bf85f60edd
afa <afa.alone@gmail.com>
2011-08-09 01:13:36 +0400

    spc

--- spec/views/accounts/edit.html.haml_spec.rb
+++ spec/views/accounts/edit.html.haml_spec.rb
@@ -1,5 +1,30 @@
 require 'spec_helper'
 
 describe "accounts/edit.html.haml" do
-  pending "add some examples to (or delete) #{__FILE__}"
+ before do
+  @user = FactoryGirl.build(:user)
+  5.times do
+   @user.accounts << FactoryGirl.create(:account, :user => @user)
+  end
+
+  @user.stub! :check_axapta_validity
+  @user.save!
+  @account = @user.accounts.first
+  assign(:user, @user)
+  #assign(:accounts, @user.accounts)
+  assign(:account, @account)
+  render
+ end
+
+ it "should render form" do
+  rendered.should have_xpath("//form[@action='#{user_account_path(@user, @account)}']")
+ end
+ it "should render contact" do
+  rendered.should have_xpath("//form//input[@type='text' and @name='account[contact_first_name]' and @value='#{@account.contact_first_name}']")
+  rendered.should have_xpath("//form//input[@type='text' and @name='account[contact_middle_name]' and @value='#{@account.contact_middle_name}']")
+  rendered.should have_xpath("//form//input[@type='text' and @name='account[contact_last_name]' and @value='#{@account.contact_last_name}']")
+ end
+ it "should render disabled axapta_hash" do
+  rendered.should have_xpath("//form//input[@type='text' and @name='account[axapta_hash]' and @value='#{@account.axapta_hash}' and @disabled]")
+ end
 end
