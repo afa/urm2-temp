18a7a8c73fd88fb7ad244a15b042851f00dfa870
afa <afa.alone@gmail.com>
2011-08-10 02:15:18 +0400

    cfg

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,5 +1,4 @@
 Urm::Application.configure do
-  puts "===test env"
   # Settings specified here will take precedence over those in config/application.rb
 
   # The test environment is used exclusively to run your application's
