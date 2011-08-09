d3285ffbd54156bb9ab8e188a0a305f14b51c596
afa <afa@afalone.local>
2011-08-09 19:09:50 +0400

    spec

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,4 +1,8 @@
 class AccountsController < ApplicationController
+
+ before_filter :get_user
+ before_filter :get_account, :only => [:show, :edit, :update, :destroy]
+ before_filter :get_accounts, :only => :index
   def index
   end
 
@@ -12,12 +16,39 @@ class AccountsController < ApplicationController
   end
 
   def create
+   if @user.accounts.create params[:account]
+    redirect_to user_accounts_path(@user)
+   else
+    @account = @user.accounts.new(params[:account] || {})
+    render :new
+   end
   end
 
   def update
+   if @account.update_attributes params[:account]
+    redirect_to user_accounts_path(@user)
+   else
+    render :edit
+   end
   end
 
   def destroy
+   if @account
+    @account.destroy
+   end
+   redirect_to user_accounts_path(@user)
   end
 
+ protected
+  def get_user
+   @user = User.find(params[:user_id])
+  end
+
+  def get_account
+   @account = @user.accounts.find params[:id]
+  end
+
+  def get_accounts
+   @accounts = @user.accounts
+  end
 end
--- spec/views/main/search.html.haml_spec.rb
+++ spec/views/main/search.html.haml_spec.rb
@@ -9,7 +9,8 @@ describe "main/search.html.haml" do
   #@login.stub!(:unique_hash).and_return(true)
   Axapta.stub!(:user_info).with(@login.ext_hash).and_return({})
   @login.save!
-  assign(:items, [])
+  @items = []
+  assign(:items, @items)
   assign(:accounts, @login.accounts)
   assign(:current_user, @login)
   #controller.send(:current_user=, @login)
@@ -41,6 +42,93 @@ describe "main/search.html.haml" do
  it "should show account selection form" do
   rendered.should have_xpath("//select[@name='hash']")
  end
- it "should show render all items" 
+ it "should show render all items" do
+  @items.each {|i| rendered.should have_content(i)  }
+ end
  #it "should show link to "
 end
+
+=begin
+  items: 
+    content: 
+      item_brend: 
+        getter: getinvcBrendAlias
+        title: производитель
+        type: string
+      item_id: 
+        getter: getItemId
+        title: код
+        type: string
+      item_name: 
+        getter: getInventItemName
+        title: наименование
+        type: string
+      locations: 
+        content: 
+          delivery_prognosis: 
+            content: 
+              delivery_date: 
+                getter: getDeliverydate
+                type: date
+              delivery_qty: 
+                getter: getDeliveryQty
+                type: date
+            iterator: nextPrognosis
+            type: array
+          location_id: 
+            getter: getInventLocationId
+            title: склады
+            type: string
+          price_qty: 
+            content: 
+              max_qty: 
+                getter: getMaxSalesQty
+                options: hide_if_empty
+                title: "макс. кол-во для цены"
+                type: integer
+              min_qty: 
+                getter: getMinSalesQty
+                title: "мин. кол-во для цены"
+                type: integer
+              price: 
+                getter: getPrice
+                title: цена
+                type: real
+              price_ref: 
+                getter: getPriceRef
+                options: hide_if_empty
+                title: "цена продажи"
+            iterator: nextPrice
+            title: "цены - количества"
+            type: array
+          vend_qty: 
+            getter: getAvailableQty
+            title: "всего в наличии"
+            type: integer
+        iterator: nextLocation
+        title: склады
+        type: array
+      min_qty: 
+        getter: getMinQty
+        title: "минимальное кол-во"
+        type: integer
+      package_name: 
+        getter: getinvcPackageName
+        title: корпус
+        type: string
+      qty_in_pack: 
+        getter: getwmscQtyInPackReq
+        title: "кол-во в упаковке"
+        type: integer
+      rohs: 
+        getter: getinvcInventItemPrefix
+        title: ROHS
+        type: string
+      segment_rus: 
+        getter: getcmpSegmentEnum
+        title: сегмент
+        type: segment_rus
+    iterator: nextItem
+    title: "результаты поиска"
+    type: array
+=end
