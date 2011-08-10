42a40fd8528d37e18e9d473a777dfe9ddd4a2aa4
afa <afa@afalone.local>
2011-08-10 12:25:07 +0400

    spc

--- spec/views/users/show.html.haml_spec.rb
+++ spec/views/users/show.html.haml_spec.rb
@@ -12,5 +12,10 @@ describe "users/show.html.haml" do
  it "should render user name" do
   rendered.should have_content(@user.username)
  end
- it "should render user name"
+ it "should render edit link" do
+  rendered.should have_xpath("//a[@href='#{edit_user_path(@user)}']")
+ end
+ it "should render accounts" do
+  @user.accounts.each {|a| rendered.should have_content(a.axapta_hash) }
+ end
 end
