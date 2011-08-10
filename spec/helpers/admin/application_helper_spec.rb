14cd96593e289582013a1dc418841c5f3d1d2ee5
afa <afa.alone@gmail.com>
2011-08-11 00:23:42 +0400

    adm

--- spec/helpers/admin/application_helper_spec.rb
+++ spec/helpers/admin/application_helper_spec.rb
@@ -1,54 +1,54 @@
 require 'spec_helper'
 
 describe Admin::ApplicationHelper do
+# before do
+#  @user = FactoryGirl.build(:user)
+#  5.times do
+#   @user.accounts << FactoryGirl.create(:account, :user => @user)
+#  end
+#
+#  @user.stub! :check_axapta_validity
+#  @user.save!
+#  assign(:user, @user)
+#  assign(:accounts, @user.accounts)
+#  render
+# end
+
  describe "on #current_user" do
   context "when user valid" do
    before do
-    @manager = FactoryGirl.build(:manager)
-    @manager.stub!(:valid?).and_return(true)
-    @manager.save!
-    assigns[:current_user] = @manager
-    #session[:manager] = @manager.id
+    @manager = FactoryGirl.create(:manager)
+    assign(:current_user, @manager)
    end
    it "should locate logged user" do
-    current_user.should_not be_nil
-    current_user.should == @manager
+    helper.current_user.should == @manager
    end
   end
  end
  describe "on #logged_in?" do
   before do
-   @manager = FactoryGirl.build(:manager)
-   @manager.stub!(:valid?).and_return(true)
-   @manager.save!
-   assigns[:current_user] = @manager
-   #session[:manager] = @manager.id
+   @manager = FactoryGirl.create(:manager)
+   assign(:current_user, @manager)
   end
   context "when valid user" do
    it "say ok" do
-    logged_in?.should be
+    helper.logged_in?.should be
    end 
   end
-  context "when unknown user" do
-   before do
-    assigns[:current_user] = 0
-   #assign(:current_user, 0)
-    #controller.current_user = 0
-    #session[:manager] = 0
-   end
-   it "say no" do
-    logged_in?.should_not be_true
-   end
-  end
+#  context "when unknown user" do
+#   before do
+#    assign(:current_user, 0)
+#   end
+#   it "say no" do
+#    helper.logged_in?.should_not be_true
+#   end
+#  end
   context "when no user" do
    before do
-    assigns[:current_user] = nil
-   #assign(:current_user, nil)
-    #controller.sign_out
-    #session[:manager] = nil
+    assign(:current_user, nil)
    end
    it "say no" do
-    logged_in?.should_not be_true
+    helper.logged_in?.should_not be_true
    end
   end
  end
