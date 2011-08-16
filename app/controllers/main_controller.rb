cd6b0a20a73bff0e66f7c5f54c1a213ed673916c
afa <afa@afalone.local>
2011-08-16 15:51:30 +0400

    src

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -19,9 +19,9 @@ class MainController < ApplicationController
 
   def dms
    respond_with do |format|
-    format.js do
-     render(:update) {|page| page.replace_html 'div.tst', '<div>asd</div>'.html_safe }
-    end
+    format.js #do
+    # render(:update) {|page| page.replace_html 'div.tst', '<div>asd</div>'.html_safe }
+    #end
     format.html do
      redirect_to root_path
     end
