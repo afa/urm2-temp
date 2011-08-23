f98d55973c93d0484989f9570a8c5959b716cf50
afa <afa@afalone.local>
2011-08-23 18:43:13 +0400

    fix

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -35,8 +35,8 @@ class MainController < ApplicationController
    @hash = current_user.current_account.try(:axapta_hash)
    @items = Axapta.search_dms_names(:user_hash => @hash, :query_string => @seek, :search_brend => @brend).inject([]) do |r, i|
     p i
-    i["locations"].each do |loc|
-     a = {"item_name" => i["item_name"], "item_brend" => i["item_brend"], "qty_in_pack" => i["qty_in_pack"], "location_id" => loc["location_id"], "min_qty" => i["min_qty"], "max_qty" => loc["vend_qty"]}
+    i["prognosis"].each do |loc|
+     a = {"item_name" => i["item_name"], "item_brend" => i["item_brend"], "qty_in_pack" => loc["qty_multiplies"], "max_qty" => loc["vend_qty"]}#, "min_qty" => i["min_qty"], "location_id" => loc["location_id"]
      locs = loc["price_qty"].sort_by{|l| l["min_qty"] }[0, 4]
      a.merge!("price1" => locs[0]["price"]) if locs[0]
      a.merge!("price2" => locs[1]["price"], "count2" => locs[1]["min_qty"]) if locs[1]
