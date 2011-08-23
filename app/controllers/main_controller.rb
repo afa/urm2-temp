4680068afd62dacec732ee628cb62b1be2a6d0ae
afa <afa@afalone.local>
2011-08-23 15:50:13 +0400

    fix

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -33,7 +33,18 @@ class MainController < ApplicationController
    @seek = params[:name]
    @brend = params[:brend]
    @hash = current_user.current_account.try(:axapta_hash)
-   @items = Axapta.search_dms_names(:user_hash => @hash, :query_string => @seek, :search_brend => @brend)
+   @items = Axapta.search_dms_names(:user_hash => @hash, :query_string => @seek, :search_brend => @brend).inject([]) do |r, i|
+    i["locations"].each do |loc|
+     a = {"item_name" => i["item_name"], "item_brend" => i["item_brend"], "qty_in_pack" => i["qty_in_pack"], "location_id" => loc["location_id"], "min_qty" => i["min_qty"], "max_qty" => loc["vend_qty"]}
+     locs = loc["price_qty"].sort_by{|l| l["min_qty"] }[0, 4]
+     a.merge!("price1" => locs[0]["price"]) if locs[0]
+     a.merge!("price2" => locs[1]["price"], "count2" => locs[1]["min_qty"]) if locs[1]
+     a.merge!("price3" => locs[2]["price"], "count3" => locs[2]["min_qty"]) if locs[2]
+     a.merge!("price4" => locs[3]["price"], "count4" => locs[3]["min_qty"]) if locs[3]
+     r << a
+    end
+    r
+   end
    respond_with do |format|
     format.js { render :layout => false } #do
     # render(:update) {|page| page.replace_html 'div.tst', '<div>asd</div>'.html_safe }
