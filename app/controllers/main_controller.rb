8f9091c7d2ef5d70f212f7fada2252f8d1527a3d
afa <afa@afalone.local>
2011-08-29 18:25:46 +0400

    fix

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -21,7 +21,7 @@ class MainController < ApplicationController
    end
    @items = data.inject([]) do |r, i|
     i["locations"].each do |loc|
-     a = {"item_name" => i["item_name"], "item_brend" => i["item_brend"], "qty_in_pack" => i["qty_in_pack"], "location_id" => loc["location_id"], "min_qty" => i["min_qty"], "max_qty" => loc["vend_qty"], "rohs" => i["rohs"], "item_id" => i["item_id"]}
+     a = {"item_name" => i["item_name"], "item_brend" => i["item_brend"], "qty_in_pack" => i["qty_in_pack"], "location_id" => loc["location_id"], "min_qty" => i["min_qty"], "max_qty" => loc["vend_qty"], "rohs" => i["rohs"], "item_id" => i["item_id"], "segment_rus" => i["segment_rus"]}
      locs = loc["price_qty"].sort_by{|l| l["min_qty"] }[0, 4]
      a.merge!("price1" => locs[0]["price"]) if locs[0]
      a.merge!("price2" => locs[1]["price"], "count2" => locs[1]["min_qty"]) if locs[1]
