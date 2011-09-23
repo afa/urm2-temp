df1e5015fac69cacf294089c2a1eda4e05735354
afa <afa@afalone.local>
2011-09-23 12:37:35 +0400

    fix

--- lib/web_utils.rb
+++ lib/web_utils.rb
@@ -4,4 +4,9 @@ module WebUtils
   scope.concat(Digest::MD5.hexdigest(str))
   #str.gsub(/[\/<>]/, '_')
  end
+
+ def self.parse_bool(str)
+  return true if %w(1 T t y Y true).include?(str)
+  false
+ end
 end
