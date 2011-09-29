81ff36d68ddc89f83302ff0a333421a62c8f4b96
afa <afa@afalone.local>
2011-09-29 12:18:55 +0400

    ci

--- goldberg_config.rb
+++ goldberg_config.rb
@@ -4,7 +4,8 @@
     #config.ruby = '1.8.7' # Your server needs to have rvm installed for this setting to be considered
     #config.environment_variables = {"FOO" => "bar"}
     #config.after_build Proc.new { |build, project| `touch ~/Desktop/actually_built`}
+    config.environment_variables = {"RAILS_ENV" => "test"}
     config.timeout = 10.minutes
-    config.command = 'bundle install&&RAILS_ENV=test rake db:migrate&&RAILS_ENV=test rake spec' #to be used if you're using anything other than rake
+    config.command = 'RAILS_ENV=test rake db:migrate&&RAILS_ENV=test rake spec' #to be used if you're using anything other than rake
   end
 
