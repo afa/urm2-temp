17586d1f880b3dddfdb31714a9fabd8e87e8a76f
afa <afa@afalone.local>
2011-09-29 18:50:57 +0400

    fix

--- public/javascripts/urm.js
+++ public/javascripts/urm.js
@@ -31,7 +31,7 @@ function load_dms_each(need_load){
 }
 
 function fix_visibility(){
- var hsh = new Hash;
+ var hsh = new Array();
  $("tr:has(.dms.js)").each(function(idx){
   hsh[$(this).attr("class").match(/\bitem_/)] = 1;
  });
