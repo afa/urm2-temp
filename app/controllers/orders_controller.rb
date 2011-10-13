9c4fc26c156b3d51df99f5e44d1ff809ba913657
afa <afa@afalone.local>
2011-10-13 16:28:09 +0400

    fix

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -30,7 +30,7 @@ class OrdersController < ApplicationController
   end
 
   def show
-   @order = Axapta.sales_lines(:sales_id => params[:id], :user_hash => current_user.current_account.axapta_hash, :only_open => true)
+   @order = Axapta.sales_lines(:sales_id => params[:id], :user_hash => current_user.current_account.axapta_hash)#, :only_open => true)
 # 2. Строки заказа
  
 # Меню: Заказы - Открытые строки
