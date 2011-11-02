3ab24682f1de365964106b43eba37746621ae3b3
afa <afa@afalone.local>
2011-11-02 18:17:14 +0400

    fix

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,13 +1,19 @@
 class CartsController < ApplicationController
  respond_to :js, :html, :json
   def index
-   @cart = CartItem.where(:user_id => current_user.id).unprocessed.all
+   @cart = CartItem.where(:user_id => current_user.id).unprocessed.in_cart.all
   end
 
   def new
   end
 
   def create
+   if params[:items][:commit]
+    params[:items].reject{|k, v| k == :commit }.reject{|k, v| v[:amount].empty? }.each do 
+    end
+    redirect_to carts_path
+   end
+   redirect_to :back
   end
 
   def edit
