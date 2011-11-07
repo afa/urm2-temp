d23c47700764c8b84df7cb905524cdaf202c52b6
afa <afa@afalone.local>
2011-11-07 16:53:29 +0400

    fix

--- app/models/cart_store.rb
+++ app/models/cart_store.rb
@@ -3,8 +3,8 @@ class CartStore < CartItem
    ::I18n::t :cart_store
   end
 
-  def self.prepare_code(current_user, search_hash)
-   hsh = {:user_id => current_user.id, :product_link => search_hash["item_id"], :product_name => search_hash["item_name"], :product_rohs => search_hash["rohs"], :product_brend => search_hash["item_brend"], :location_link => search_hash["location_id"]}
+  def self.prepare_code(search_hash)
+   hsh = {:user_id => User.current.id, :product_link => search_hash["item_id"], :product_name => search_hash["item_name"], :product_rohs => search_hash["rohs"], :product_brend => search_hash["item_brend"], :location_link => search_hash["location_id"]}
    fnd = self.unprocessed.where( hsh ).order("updated_at desc").all
    if fnd.empty?
     fnd << self.create(hsh.merge(:draft => true, :processed => false, :avail_amount => search_hash["max_qty"], :min_amount => search_hash["min_qty"]))
@@ -13,7 +13,7 @@ class CartStore < CartItem
    unless fnd.empty?
     fnd.each{|i| i.destroy }
    end
-   item.update_attributes(:avail_amount => search_hash["max_qty"], :min_amount => search_hash["min_qty"])
+   item.update_attributes(:avail_amount => search_hash["max_qty"], :min_amount => search_hash["min_qty"], :max_amount => search_hash["locations"].first["vend_qty"], :quantity => search_hash["qty_in_pack"])
    search_hash.merge(:cart_id => item.id)
    #search_hash.merge()
    
