afd6577d9136a69409931b39b8797c4cb1043087
afa <afa@afalone.local>
2011-11-09 14:11:15 +0400

    fix

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -63,7 +63,7 @@ class MainController < ApplicationController
   def conv_dms_items(items)
    items.inject([]) do |r, i|
     i["prognosis"].each do |loc|
-     a = {"item_name" => i["item_name"], "item_brend" => i["item_brend"], "qty_in_pack" => loc["qty_multiples"], "max_qty" => loc["vend_qty"], "rohs" => i["rohs"], "prognosis_id" => loc["prognosis_id"]}#, "min_qty" => i["min_qty"], "location_id" => loc["location_id"]
+     a = {"item_name" => i["item_name"], "item_brend" => i["item_brend"], "qty_multiples" => loc["qty_multiples"], "max_qty" => loc["vend_qty"], "rohs" => i["rohs"], "prognosis_id" => loc["prognosis_id"]}#, "min_qty" => i["min_qty"], "location_id" => loc["location_id"]
      locs = loc["price_qty"].sort_by{|l| l["min_qty"] }[0, 4]
      a.merge!("price1" => locs[0]["price"], "min_qty" => locs[0]["min_qty"], "vend_proposal_date" => locs[0]["vend_proposal_date"]) if locs[0]
      a.merge!("price2" => locs[1]["price"], "count2" => locs[1]["min_qty"]) if locs[1]
