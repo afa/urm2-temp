38c0e0a1e43e52cdeaffd967e4aba7c594fa0c92
afa <afa@afalone.local>
2011-11-09 14:17:55 +0400

    fix

--- app/models/axapta_request.rb
+++ app/models/axapta_request.rb
@@ -1,5 +1,6 @@
 require 'json_rpc_client'
 class AxaptaRequest < JsonRpcClient
     #json_rpc_service 'http://192.168.0.72:20013/jsonrpc2', :no_auto_config => true
-    json_rpc_service 'http://192.168.0.72:20013/jsonrpc', :no_auto_config => true
+    #json_rpc_service 'http://192.168.0.72:20013/jsonrpc', :no_auto_config => true
+    json_rpc_service 'http://172.16.200.40/jsonrpc_test', :no_auto_config => true
 end
