a13dd46d12549c368618da0df49fb3b1823aca34
afa <afa.alone@gmail.com>
2011-11-24 20:45:42 +0400

    fix

--- spec/factories/cart.rb
+++ spec/factories/cart.rb
@@ -4,16 +4,22 @@ FactoryGirl.define do
   n.to_s.rjust(8)
  end
  factory  :cart, :class => CartItem do
+  ignore do
+   offers_count 3
+  end
   product_name "Base"
   product_link { next(:product_code) }
   factory :cart_store do
    type 'CartStore'
    product_name "store"
+   offer_params { {"price_qty" => offers_count.times{|i| {"min_qty" => i*100+50, "price" => 5.0-(i*0.5)} } } }
   end
   factory :cart_world do
    type "CartWorld"
    product_name "store"
   end
+  after_build {|c| c.serialize_offer }
+  after_build {|c| c.setup_price }
  end 
 
 end
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -15,14 +15,20 @@ describe Account do
  context "on renew_structure" do
    let(:ui) { {:blocked => '0', :business => "КОМПЭЛ", :empl_name=>"КАШИНА НАТАЛЬЯ ЛЬВОВНА", :empl_email => 'datis@compel.ru', :contact_email => 'datis@compel.ru', :contact_first_name => 'Владимир', :contact_last_name => 'Ластовка', :contact_middle_name => 'Владимирович', :user_name => 'УРМ-2 Тест' } } #, :user_id => 1
   before do
-   User.any_instance.stub(:valid?).and_return(true)
+   @user = FactoryGirl.build(:user)
+   @user.stub!(:valid?).and_return(true)
+   @user.stub!(:create_axapta_account).and_return(true)
    Axapta.stub!(:user_info).and_return(ui)#, :user_id => 1
-   @user = FactoryGirl.create(:user)
-   @account = Account.find_by_axapta_hash(@user.ext_hash)
-   @account.update_attributes :axapta_user_id => Factory.next(:axapta_user_id)
+   @user.save!
+   @account = FactoryGirl.build(:account, :blocked => false, :user => @user)
+   @account.save!
+   @user.current_account = @user.accounts.first
+   @user.save!
+
+   #@account.update_attributes :axapta_user_id => Factory.next(:axapta_user_id)
    #@account = FactoryGirl.create(:account, :user => @user, :axapta_hash => 'asdfg')
-   Axapta.stub!(:load_child_hashes).with(@user.ext_hash).and_return([])
-   Axapta.renew_structure(@user.ext_hash)
+   Axapta.stub!(:load_child_hashes).with(@account.axapta_hash).and_return([])
+   Axapta.renew_structure(@account.axapta_hash)
   end
   subject { @account }
   it "should reload axapta info" do
@@ -31,17 +37,21 @@ describe Account do
   end
   context "should renew parent" do
    before do
-    #User.any_instance.stub(:valid?).and_return(true)
-    #Axapta.stub(:user_info).with('asdfg').and_return(:blocked => '0', :business => "КОМПЭЛ", :empl_name=>"КАШИНА НАТАЛЬЯ ЛЬВОВНА", :empl_email => 'datis@compel.ru', :contact_email => 'datis@compel.ru', :contact_first_name => 'Владимир', :contact_last_name => 'Ластовка', :contact_middle_name => 'Владимирович', :parent_user_id => '', :user_name => 'УРМ-2 Тест')
-    @puser = FactoryGirl.create(:user)
-    @prnt = Account.find_by_axapta_hash(@puser.ext_hash)
+    @puser = FactoryGirl.build(:user)
+    @puser.stub!(:valid?).and_return(true)
+    @puser.stub!(:create_axapta_account).and_return(true)
+    Axapta.stub!(:user_info).and_return(ui)#, :user_id => 1
+    @puser.save!
+    @prnt = FactoryGirl.build(:account, :blocked => false, :user => @puser)
+    @prnt.save!
+    @puser.current_account = @puser.accounts.first
+    @puser.save!
     @prnt.children << @account
-    #@account = FactoryGirl.create(:account, :user => @user, :axapta_hash => 'asdfg')
     @account.axapta_parent_id = @prnt.axapta_user_id
     @account.save!
-    Axapta.stub!(:user_info).with(@user.ext_hash).and_return({})
-    Axapta.stub!(:user_info).with(@puser.ext_hash).and_return({:business => 'test'})
-    Axapta.renew_structure(@user.ext_hash)
+    Axapta.stub!(:user_info).with(@user.current_account.axapta_hash).and_return({})
+    Axapta.stub!(:user_info).with(@puser.current_account.axapta_hash).and_return({:business => 'test'})
+    Axapta.renew_structure(@user.current_account.axapta_hash)
    end
    it "with hash" do
     Account.find(@prnt.id).business.should == 'test'
@@ -49,19 +59,28 @@ describe Account do
   end
   context "should renew children" do
    before do
-    #User.any_instance.stub(:valid?).and_return(true)
-    #Axapta.stub(:user_info).with('asdfg').and_return(:blocked => '0', :business => "КОМПЭЛ", :empl_name=>"КАШИНА НАТАЛЬЯ ЛЬВОВНА", :empl_email => 'datis@compel.ru', :contact_email => 'datis@compel.ru', :contact_first_name => 'Владимир', :contact_last_name => 'Ластовка', :contact_middle_name => 'Владимирович', :parent_user_id => '', :user_name => 'УРМ-2 Тест')
-    @uchld = FactoryGirl.create_list(:user, 2)
-    @chld = @uchld.map{|u| Account.find_by_axapta_hash(u.ext_hash)}.uniq
-    @account.children << @chld
-    @account.children.each{|a| a.axapta_parent_id = @account.axapta_user_id }
-    @account.children.each{|a| a.axapta_user_id = Factory.next(:axapta_user_id) }
-    @account.children.each{|a| a.save! }
-    Axapta.stub!(:user_list).with(@user.ext_hash).and_return({})
+    @uchld = FactoryGirl.build_list(:user, 2)
+    Axapta.stub!(:user_info).and_return(ui)#, :user_id => 1
+    @chld = []
+    @uchld.each do |u|
+     u.stub!(:valid?).and_return(true)
+     u.stub!(:create_axapta_account).and_return(true)
+     u.save!
+     a = FactoryGirl.build(:account, :blocked => false, :user => u)
+     a.save!
+     u.current_account = u.accounts.first
+     u.save!
+     @chld << a
+     @account.children << a
+     a.axapta_parent_id = @account.axapta_user_id
+     a.save!
+    end
+    #@account.children.each{|a| a.axapta_user_id = Factory.next(:axapta_user_id) }
+    Axapta.stub!(:user_list).with(@user.current_account.axapta_hash).and_return({})
     #@chld.each{|u| Axapta.stub!(:user_info).with(u.axapta_hash).and_return({:business => 'test'}) }
-    Axapta.stub!(:load_child_hashes).with(@user.ext_hash).and_return(@user.accounts.first.children.map{|c| {"business" => 'tst', "user_id" => c.axapta_user_id} })
+    Axapta.stub!(:load_child_hashes).with(@user.current_account.axapta_hash).and_return(@user.accounts.first.children.map{|c| {"business" => 'tst', "user_id" => c.axapta_user_id} })
     @chld.each {|c| Axapta.stub!(:user_info).with(c.axapta_hash).and_return(:business => 'tst') }
-    Axapta.renew_structure(@user.ext_hash)
+    Axapta.renew_structure(@user.current_account.axapta_hash)
    end
    it "with hash" do
     @chld.each{|a| Account.find(a.id).business.should == 'tst' }
