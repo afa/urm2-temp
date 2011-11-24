88959f0786a30e8a65640e29ac9bccd2a5534b95
afa <afa@afalone.local>
2011-11-24 13:25:26 +0400

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -100,8 +100,22 @@ class User < ActiveRecord::Base
   def make_order(dead_line, delivery)
    reqs = cart_items.unprocessed.in_cart.all.partition{|c| c.is_a?(CartRequest) }
    res = []
-   res << Axapta.make_order(:sales_lines => reqs[1].map{|i| i.to_sales_lines }, :date_dead_line => dead_line, :customer_delivery_type_id => delivery) unless reqs[1].empty?
-   res << Axapta.make_order(:sales_lines => reqs[0].map{|i| i.to_sales_lines }, :date_dead_line => dead_line) unless reqs[0].empty?
+   unless reqs[1].empty?
+    begin
+     res << Axapta.make_order(:sales_lines => reqs[1].map{|i| i.to_sales_lines }, :date_dead_line => dead_line, :customer_delivery_type_id => delivery)
+     reqs[1].each{|i| i.update_attributes :processed => true, :order => res.last }
+    rescue Exception => e
+     
+    end
+   end
+   unless reqs[0].empty?
+    begin
+     res << Axapta.make_order(:sales_lines => reqs[0].map{|i| i.to_sales_lines }, :date_dead_line => dead_line)
+     reqs[0].each{|i| i.update_attributes :processed => true, :order => res.last }
+    rescue Exception => e
+     
+    end
+   end
    res
   end
 
