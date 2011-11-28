84991e08364ecff1c3e837100bfc7239dfaea666
afa <afa@afalone.local>
2011-11-28 13:33:44 +0400

    fix

--- app/models/cart_request.rb
+++ app/models/cart_request.rb
@@ -61,6 +61,23 @@ class CartRequest < CartItem
    Axapta.search_names(:calc_price => true, :calc_qty => true, :show_delivery_prognosis => true, :item_id_search => product_link, :invent_location_id => location_link, :user_hash => User.current.current_account.axapta_hash)
   end
 
+  def self.prepare_code(search) #on find, chg search hash to offers array
+   #FIXME: fix for generation
+   hsh = {:user_id => User.current.id, :product_link => search.code, :product_name => search.name, :product_rohs => search.rohs, :product_brend => search.brend, :location_link => search.location_id}
+   fnd = self.unprocessed.where( hsh ).order("updated_at desc").all
+   #if fnd.empty?
+    item = self.create(hsh.merge(:draft => true, :processed => false, :max_amount => search.max_qty, :min_amount => search.min_qty, :offer_params => search.raw_location))
+    item.offer_params.merge!(search.raw_location)
+    item.save!
+   #else
+    #item = self.create(hsh.merge(:draft => true, :processed => false, :max_amount => search.max_qty, :min_amount => search.min_qty, :quantity => search.qty_in_pack, :offer_params => search.raw_location))
+   #end #found/created
+   fnd.each{|i| i.destroy }
+   #item.update_attributes(:max_amount => search_hash["max_qty"], :min_amount => search_hash["min_qty"], :quantity => search_hash["qty_in_pack"])
+   search.cart_id = item.id
+
+  end
+
   def self.prepare_for(count, hsh)
    return CartStore.prepare_for(count, hsh) if !hsh.blank? and count <= hsh["locations"].first["vend_qty"]
    p hsh, count
