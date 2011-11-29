61d6eb64aafd9e8c643049edae6b64ee37fd0689
afa <afa@afalone.local>
2011-11-29 11:52:42 +0400

    fix

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -8,6 +8,7 @@ class CartsController < ApplicationController
   end
 
   def create
+
    if params[:items].try(:[], :commit)
     params[:items].reject{|k, v| k == :commit }.reject{|k, v| v[:amount].blank? }.each do |k, v|
      CartStore.copy_on_write(v)
@@ -20,6 +21,14 @@ class CartsController < ApplicationController
      end
     end
     redirect_to carts_path
+   respond_with do |format|
+    format.json do
+     render :json => {:dms => render_to_string( :partial => "main/dms_block.html", :locals => {:items => @items, :after => @after} ), :gap => render_to_string( :partial => "main/gap_line.html", :locals => {:after => @after}), :empty => render_to_string(:partial => "main/dms_empty.html", :locals => {:after => @after})}
+    end
+    format.js { render :layout => false }
+    format.html do
+     redirect_to root_path
+    end
    else
     redirect_to :back
    end
