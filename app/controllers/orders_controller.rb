494b69d0079e026e676b313fff51d9a91ca9ac5e
afa <afa@afalone.local>
2011-11-29 18:50:04 +0400

    fix

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,6 @@
 class OrdersController < ApplicationController
 
+ respond_to :html, :js
  before_filter :get_filter
 
   def index
@@ -37,6 +38,16 @@ class OrdersController < ApplicationController
    @reqs = @carts.partition{|i| i.is_a? CartRequest }[0]
    @nreqs = @carts.partition{|i| i.is_a? CartRequest }[1]
    @deliveries = User.current.deliveries
+   respond_with do |format|
+    #format.json do
+    # render :json => {:dms => render_to_string( :partial => "main/dms_block.html", :locals => {:items => @items, :after => @after} ), :gap => render_to_string( :partial => "main/gap_line.html", :locals => {:after => @after}), :empty => render_to_string(:partial => "main/dms_empty.html", :locals => {:after => @after})}
+    #end
+    format.js { render :layout => false }
+    format.html do
+     render
+    end
+   end
+
   end
 
   def create
