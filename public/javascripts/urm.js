ffed9edaba2554e63a96122cb11d1473eeed0b6f
afa <afa@afalone.local>
2011-12-08 19:34:29 +0400

    fix

--- public/javascripts/urm.js
+++ public/javascripts/urm.js
@@ -1,12 +1,12 @@
 /* helper for URM2 */
 
 Array.prototype.getUnique = function () {
-var o = new Object();
-var i, e;
-for (i = 0; e = this[i]; i++) {o[e] = 1};
-var a = new Array();
-for (e in o) {a.push (e)};
-return a;
+ var o = new Object();
+ var i, e;
+ for (i = 0; e = this[i]; i++) {o[e] = 1};
+ var a = new Array();
+ for (e in o) {a.push (e)};
+ return a;
 } 
 
 function makeAjaxCall(ajaxUrl, functionSuccess, functionFailure){
