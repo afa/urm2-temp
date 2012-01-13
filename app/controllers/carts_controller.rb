4c5a5e961ae6b21286b66716fc917dea22519ba2
afa <afa@afalone.local>
2012-01-13 18:29:05 +0400

    fix

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -90,7 +90,7 @@ class CartsController < ApplicationController
    @deliveries = User.current.deliveries
    respond_with do |format|
     format.js { render :layout => false }
-    format.json { render :json => {:carts_table => escape_javascript(render_string(:partial => "carts/cart_table", :locals => {:cart => @carts})), :old => @old, :new => @new, :carts_empty => @carts.empty?} }
+    format.json { render :json => {:carts_table => escape_javascript(render_to_string(:partial => "carts/cart_table", :locals => {:cart => @carts})), :old => @old, :new => @new, :carts_empty => @carts.empty?} }
    end
   end
 
