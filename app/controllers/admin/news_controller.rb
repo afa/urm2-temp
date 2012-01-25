581c109fdec546f8773e4a3be221965730a37957
afa <afa@afalone.local>
2012-01-25 17:46:32 +0400

    fix

--- app/controllers/admin/news_controller.rb
+++ app/controllers/admin/news_controller.rb
@@ -8,7 +8,18 @@ class Admin::NewsController < Admin::ApplicationController
   def new
   end
 
+  def create
+   NewsArticle.create params[:news_article]
+   redirect_to :admin_root_path
+  end
+
   def edit
+  end
+
+  def update
+  end
+
+  def destroy
   end
 
 end
