f0d2f64e150d4b54886105fd2048f94d381b3244
afa <afa@afalone.local>
2012-01-27 17:22:08 +0400

    fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,4 +6,18 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require_tree .
+//= require jquery.ui.core
+//= require jquery.header
+//= require jquery.controls
+//= require jquery.table
+//= require jquery.lightbox
+//= require date
+//= require jquery.ui.datepicker
+//= require urm
+//= require jquery.validationEngine-ru
+//= require jquery.validationEngine
+//= require jquery-ui-i18n
+//= require jquery.ui.datepicker-ru
+//= require jquery.watch
+
+//-# = require_tree .
