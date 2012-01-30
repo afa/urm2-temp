dfd1ad49bd3943fa0eab1daeca401d6408642b48
afa <afa@afalone.local>
2012-01-30 15:09:27 +0400

    fix

--- config/initializers/sass_config.rb
+++ config/initializers/sass_config.rb
@@ -1,6 +1,7 @@
 require 'sass/plugin'
 Urm::Application.configure do
     # Sass
-    config.sass.template_location = {"assets/stylesheets/sass" => "assets/stylesheets"}
+    config.sass.remove_template_location(File.join(Rails.root, "public", "stylesheets", "sass"))
+    config.sass.add_template_location(File.join(Rails.root, "assets", "stylesheets", "sass"), File.join(Rails.root, "assets", "stylesheets"))
 end
 
