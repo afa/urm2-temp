9164e289321dca2aee2068d0ba625c220c2b9381
afa <afa@afalone.local>
2012-03-14 16:38:56 +0400

    fix

--- app/assets/javascripts/urm.js
+++ app/assets/javascripts/urm.js
@@ -274,7 +274,7 @@ function ordersSaveOnClick(){
  var curr = this;
  $('input[id^="order_"][id$="_note"]').each(function(i, item){ ordersCopyToHidden(item, curr); });
  $('input[id^="order_"][id$="_requirements"]').each(function(i, item){ ordersCopyToHidden(item, curr); });
- $('input[id^="order_"][id$="_comment"]').each(function(i, item){ ordersCopyToHidden(item, curr); });
+ $('textarea[id^="order_"][id$="_comment"]').each(function(i, item){ ordersCopyToHidden(item, curr); });
  //$(this).parents("form").submit();
  return false;
 }
