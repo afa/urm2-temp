28b79ec2c54cef45bc32edb8435217dace3ec2fc
afa <afa@afalone.local>
2012-03-14 15:54:10 +0400

    fix

--- app/assets/javascripts/urm.js
+++ app/assets/javascripts/urm.js
@@ -274,6 +274,7 @@ function ordersSaveOnClick(){
  $('input[id^="order_"][id$="_requirements"]').each(function(i, item){ ordersCopyToHidden(item); });
  $('input[id^="order_"][id$="_comment"]').each(function(i, item){ ordersCopyToHidden(item); });
  //$(this).parents("form").submit();
+ alert("ttt");
  return false;
 }
 
