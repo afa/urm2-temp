c531efe228b06bf02e6c1c94c4c8d39d45853349
afa <afa@afalone.local>
2012-03-19 10:55:04 +0400

    fix

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -4,7 +4,7 @@ class CartItem < ActiveRecord::Base
  scope :unprocessed, where(:processed => false)
  scope :in_cart, where("amount > 0")
  #scope :in_cart, where(:draft => false)
- ATTR_KEYS = %w(amount product_link location_link product_name product_rohs product_brend processed order current_price prognosis quantity min_amount max_amount comment user_price actions draft offer_params offer_serialized reserve pick requirement).map(&:to_sym)
+ ATTR_KEYS = %w(amount product_link location_link product_name product_rohs product_brend processed order current_price prognosis quantity min_amount max_amount comment user_price actions draft offer_params offer_serialized reserve pick).map(&:to_sym)
 
  attr_accessor :allow
  attr_accessor :offer_params
