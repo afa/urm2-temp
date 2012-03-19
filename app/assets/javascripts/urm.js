7e84573f5bf5184e1d2f81671fd359e86bf4c822
afa <afa@afalone.local>
2012-03-19 18:13:22 +0400

    fix

--- app/assets/javascripts/urm.js
+++ app/assets/javascripts/urm.js
@@ -349,7 +349,8 @@ function ordersSaveProcess(){
 function ordersReserveOnClick(){
  var curr = this;
  $('input[name^="order["][name$="][item_id]"]').each(function(i, item){ ordersCopyToHidden(item, curr); });
- $('td.reserve_data input[type="text"][name^="order["][name$="][process_qty]"]').each(function(i, item){ if($(item).val().match(/^\d+$/)){ ordersCopyToHidden(item, curr);} });
+ $('td.reserve_data input[type="text"][name^="order["][name$="][process_qty]"]').each(function(i, item){ ordersCopyToHidden(item, curr); });
+ //$('td.reserve_data input[type="text"][name^="order["][name$="][process_qty]"]').each(function(i, item){ if($(item).val().match(/^\d+$/)){ ordersCopyToHidden(item, curr);} });
  $(this).parents("form").submit();
 }
 
