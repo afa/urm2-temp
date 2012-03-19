20e3554b952f0145c8100eef6cc5677d90651838
afa <afa@afalone.local>
2012-03-19 15:53:58 +0400

    fix

--- app/assets/javascripts/urm.js
+++ app/assets/javascripts/urm.js
@@ -323,9 +323,9 @@ function ordersCopyToHidden(item, after){
 
 function ordersSaveOnClick(){
  var curr = this;
- $('input[id^="order_"][id$="_item_id"]').each(function(i, item){ ordersCopyToHidden(item, curr); });
- $('input[id^="order_"][id$="_note"]').each(function(i, item){ ordersCopyToHidden(item, curr); });
- $('input[id^="order_"][id$="_requirement"]').each(function(i, item){ ordersCopyToHidden(item, curr); });
+ $('input[name^="order["][name$="][item_id]"]').each(function(i, item){ ordersCopyToHidden(item, curr); });
+ $('td.main_options_data input[type="text"][name^="order["][name$="][note]"]').each(function(i, item){ ordersCopyToHidden(item, curr); });
+ $('td.main_options_data input[type="text"][name^="order["][name$="][requirement]"]').each(function(i, item){ ordersCopyToHidden(item, curr); });
  $('textarea[id^="order_"][id$="_comment"]').each(function(i, item){ ordersCopyToHidden(item, curr); });
  $(this).parents("form").submit();
 }
