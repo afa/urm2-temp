7f1afad1ca021e7292c52bc2bb3af03f0ca87561
afa <afa@afalone.local>
2012-03-27 15:38:27 +0400

    fix

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,6 +30,6 @@ Urm::Application.configure do
  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
   # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.debug = false
 end
 
