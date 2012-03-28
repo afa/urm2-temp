2a8068ccfb69f93ca2946daa9e05a7385055050d
afa <afa@afalone.local>
2012-03-28 13:56:35 +0400

    fix

--- lib/array_utils.rb
+++ lib/array_utils.rb
@@ -1,8 +1,16 @@
 class Array
  def as_hash
   hsh = {}
-  self.each_slice(2){|i| hsh.merge!(i.first => i.last) }.inject({}){|r, i| r.merge(i) }
+  p "array to hash", self
+  self.each_slice(2){|i| hsh.merge!(i.first => i.last) }
+  p "array to hash rez", hsh
   hsh
  end
 end
 
+class Hash
+ def as_hash
+  p "hash to hash", self
+  self
+ end
+end
