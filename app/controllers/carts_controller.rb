8b559fd996092f80e12550d5c4b1622b8b282b5b
afa <afa@afalone.local>
2012-03-29 12:07:57 +0400

    fix

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -23,9 +23,8 @@ class CartsController < ApplicationController
      @carts = current_user.cart_items.unprocessed.in_cart.all
      @carts.each do |cart|
       cart.line = view_context.escape_javascript(render_to_string :partial => "carts/cart_line", :locals => {:cart_line => cart})
-      p "rendered line ", cart.line
      end
-     gon.carts = @carts
+     gon.carts = @carts.map(&:to_hash)
      @deliveries = User.current.deliveries
      #@carts = CartItem.where(:user_id => current_user.id).unprocessed.in_cart.all
     end
