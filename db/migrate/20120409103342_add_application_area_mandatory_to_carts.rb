a73fa1b398e31143cf456f3a1f4a7759eeda1178
afa <afa@afalone.local>
2012-04-09 16:37:07 +0400

    fix

--- db/migrate/20120409103342_add_application_area_mandatory_to_carts.rb
+++ db/migrate/20120409103342_add_application_area_mandatory_to_carts.rb
@@ -1,5 +1,6 @@
 class AddApplicationAreaMandatoryToCarts < ActiveRecord::Migration
   def change
-    add_column :cart_items, :application_area_mandatory, :string
+    add_column :cart_items, :application_area_mandatory, :boolean
+    #add_column :cart_items, :application_area_id, :string
   end
 end
