5a05e2dbf25eab7cbc7b6617eed5ec5bd7f85e0f
afa <afa@afalone.local>
2012-05-30 14:35:53 +0400

    fix

--- app/models/axapta_request.rb
+++ app/models/axapta_request.rb
@@ -3,5 +3,5 @@ class AxaptaRequest < JsonRpcClient
     #json_rpc_service 'http://192.168.0.72:20013/jsonrpc2', :no_auto_config => true
     #json_rpc_service 'http://192.168.0.72:20013/jsonrpc', :no_auto_config => true
     # nginx json_rpc_service 'http://172.16.200.40/jsonrpc_test', :no_auto_config => true
-    json_rpc_service 'http://192.168.0.72:7077/', :no_auto_config => true
+    json_rpc_service 'http://192.168.0.72:7077/', :no_auto_config => true, :uri_encode_post_bodies => true
 end
