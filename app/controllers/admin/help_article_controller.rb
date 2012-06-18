b7442b4534ec32157a90a19c2184058fd5507de2
afa <afa@afalone.local>
2012-06-18 17:25:33 +0400

    fix

--- app/controllers/admin/help_article_controller.rb
+++ app/controllers/admin/help_article_controller.rb
@@ -9,14 +9,14 @@ class Admin::HelpArticleController < Admin::ApplicationController
   end
 
   def update
-   redirect_to :index
+   redirect_to admin_help_article_path(params[:id])
   end
 
   def new
   end
 
   def create
-   redirect_to :index
+   redirect_to admin_help_article_index_path
   end
 
 end
