d97514b26ff4bbaa314cf4f3ee66cb88603895af
afa <afa@afalone.local>
2012-07-10 16:30:56 +0400

    fix

--- app/controllers/admin/main_controller.rb
+++ app/controllers/admin/main_controller.rb
@@ -2,6 +2,7 @@ class Admin::MainController < Admin::ApplicationController
   def index
    @users = User.all
    @news = NewsArticle.order("created_at desc").all
+   @articles = HelpArticle.order("key asc").all
   end
 
 end
