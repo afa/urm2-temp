2d041842256c20b2b672b71666658fd7806f99de
afa <afa@afalone.local>
2012-07-16 14:02:53 +0400

    fix

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -37,8 +37,8 @@ class CartItem < ActiveRecord::Base
 
   def self.export(format)
    parms = EXPORTABLE_FIELDS.transpose
-   p "---export", parms, FORMATTER[format](parms[1], User.current.cart_items.unprocessed.in_cart.all.map{|i| parms[0].map{|j| i.send(j) } })
-   FORMATTER[format](parms[1], User.current.cart_items.unprocessed.in_cart.all.map{|i| parms[0].map{|j| i.send(j) } })
+   p "---export", parms, FORMATTER[format].call(parms[1], User.current.cart_items.unprocessed.in_cart.all.map{|i| parms[0].map{|j| i.send(j) } })
+   FORMATTER[format].call(parms[1], User.current.cart_items.unprocessed.in_cart.all.map{|i| parms[0].map{|j| i.send(j) } })
   end
 
   def action
