7ac298bcbf23eb575b0cc5af0dae0f141e897b64
afa <afa@afalone.local>
2012-07-17 16:43:02 +0400

    fix

---
+++ app/models/exportable.rb
@@ -0,0 +1,34 @@
+module Exportable
+ def self.included(base)
+  base.extend(ClassMethods)
+  class_eval do
+   FORMATTER = {
+    :csv => proc {|csv, hdr, data|
+     csv << hdr
+     data.each{|i| csv << i }
+    }
+   }
+
+   EXPORTABLE_FIELDS = {
+    :csv => {
+     :cart =>[
+      [:type, "Тип"], [:product_name, "Наименование"], [:product_brend, "Производитель"], [:product_rohs, "ROHS"], [:current_price, "Цена"], [:quantity, "Количество"], [:location_link, "Склад"], [:prognosis, "Прогноз"], [:comment, "Примечание"], [:requirement, "Requirement"], [:user_price, "Цена клиента"], [:application_area_mandatory, "Требовать указать применение"], [:action, "Действие"]
+     ]
+    }
+   }
+  end
+ end
+ 
+ module ClassMethods
+
+  def export(format, obj)
+   parms = EXPORTABLE_FIELDS[format][obj].transpose
+   out = CSV.generate( {:col_sep => ";"}) do |csv|
+   # p "---export", parms, FORMATTER[format].call(parms[1], User.current.cart_items.unprocessed.in_cart.all.map{|i| parms[0].map{|j| i.send(j) } })
+    FORMATTER[format].call(csv, parms[1], User.current.cart_items.unprocessed.in_cart.all.map{|i| parms[0].map{|j| i.send(j) } })
+   end
+   out.force_encoding('UTF-8').encode('Windows-1251')
+  end
+
+ end
+end
