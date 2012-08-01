842fe38fb7f91518ff60ca04f850a52ea39e4b43
afa <afa@afalone.local>
2012-08-01 18:23:28 +0400

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -86,8 +86,9 @@ class UsersController < ApplicationController
    @filter.date_from = 1.month.ago.strftime("%Y-%m-%d") if @filter.date_from.blank?
    @filter_hash.merge!(:date_to => @filter.date_to, :date_from => @filter.date_from)
    respond_with do |format|
-   format.csv do
-    send_data User.export(:csv, :balance, Axapta.info_cust_trans(@filter_hash)), :type => "application/csv", :disposition => :attachment
+    format.csv do
+     send_data User.export(:csv, :balance, Axapta.info_cust_trans(@filter_hash)), :type => "application/csv", :disposition => :attachment
+    end
    end
   end
 
