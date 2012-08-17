c2d0ca7ea504466560e91325a816eb9542f6a741
afa <afa@afalone.local>
2012-08-17 18:23:58 +0400

    fix

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -1,4 +1,9 @@
 Urm::Application.configure do
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
