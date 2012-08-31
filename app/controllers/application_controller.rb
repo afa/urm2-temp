e6e38c2105fd1c57f4c912cb1ee70e2c4d24a35f
afa <afa@afalone.local>
2012-08-31 15:18:02 +0400

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   remembered_cookie_name :user_remember_token
   user_model User
   auth_expired_in_days 1
-  redirect_failed new_sessions_path
+  redirect_failed_cb lambda { new_sessions_path }
 
 
  protected
