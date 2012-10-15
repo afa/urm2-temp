3733cfaff0dcbc3d5e9a56c3708a8aa04cc585de
afa <afa@afalone.local>
2012-10-15 16:34:16 +0400

    fix

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,20 +1,3 @@
 class Admin::SessionsController < Admin::ApplicationController
- skip_before_filter :authenticate!, :only => [:new, :create]
-
-  def new
-  end
-
-  def create
-   sign_in(Manager.authenticate(params[:session].try(:[], :name), params[:session].try(:[], :password))) unless logged_in?
-   redirect_to admin_root_path if logged_in?
-   redirect_to new_admin_session_path unless logged_in?
-  end
-
-  def destroy
-   if logged_in?
-    sign_out
-   end
-   redirect_to new_admin_session_path
-  end
-
+ include Afauth::Controller::Session
 end
