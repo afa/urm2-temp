6d028c89fbdc98d430e469734546f735714c341a
afa <afa@afalone.local>
2012-10-17 15:33:39 +0400

    fizx

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
- def current_user
-  controller.current_user# ||= User.find_by_id(session[:user])
- end
- def logged_in?
-  controller.logged?
- end
+ #def current_user
+ # controller.current_user# ||= User.find_by_id(session[:user])
+ #end
+ #def logged_in?
+ # controller.logged?
+ #end
 end
