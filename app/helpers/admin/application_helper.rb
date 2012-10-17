0be6a9456288bbc3a6662303f6ef696b26ea626a
afa <afa@afalone.local>
2012-10-17 15:52:59 +0400

    fix

--- app/helpers/admin/application_helper.rb
+++ app/helpers/admin/application_helper.rb
@@ -1,14 +1,9 @@
 module Admin::ApplicationHelper
  include Afauth::Controller::Helper
-# def user_from_cookie
-#  if token = cookies[:manager_remember_token]
-#   Manager.find_by_remember_token(token)
-#  end
-# end
-# def current_user
-#  controller.current_user
-# end
-# def logged_in?
-#  not controller.current_user.blank?
-# end
+ def current_user
+  controller.current_user
+ end
+ def logged_in?
+  not controller.current_user.blank?
+ end
 end
