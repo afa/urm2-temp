fc9f916f468c6085d4a6c915b7d36fee103536f8
afa <afa@afalone.local>
2012-10-18 18:28:19 +0400

    fix

--- lib/afauth.rb
+++ lib/afauth.rb
@@ -136,16 +136,17 @@ module Afauth
      before_filter :process_cookie
      before_filter :login_from_cookie
      before_filter :authenticate!
-     #rescue_from Afauth::AuthError do |e|
-     # Rails.logger.info "---rescue: need redirect, #{e}, #{e.backtrace.first(3)}"
-     # if self.class.class_variable_defined?(:@@auth_redirect_on_failed) && self.class.auth_redirect_on_failed
-     #  redirect_to self.class.auth_redirect_on_failed
-     # elsif self.class.class_variable_defined?(:@@auth_redirect_on_failed_cb) && self.class.auth_redirect_on_failed_cb
-     #  redirect_to self.send(self.class.auth_redirect_on_failed_cb)
-     # else
-     #  raise
-     # end
-     #end
+     # hide when hardly test
+     rescue_from Afauth::AuthError do |e|
+      Rails.logger.info "---rescue: need redirect, #{e}, #{e.backtrace.first(3)}"
+      if self.class.class_variable_defined?(:@@auth_redirect_on_failed) && self.class.auth_redirect_on_failed
+       redirect_to self.class.auth_redirect_on_failed
+      elsif self.class.class_variable_defined?(:@@auth_redirect_on_failed_cb) && self.class.auth_redirect_on_failed_cb
+       redirect_to self.send(self.class.auth_redirect_on_failed_cb)
+      else
+       raise
+      end
+     end
     end
    end
 
