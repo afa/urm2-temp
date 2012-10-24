ae563a0a0e3656f46bb8d2d5eaaa6a7b59c5398f
afa <afa@afalone.local>
2012-10-24 18:48:04 +0400

    fix

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,3 +1,10 @@
 class Setting < ActiveRecord::Base
+ require "yaml"
  belongs_to :settingable, :polymorphic => true
+  def self.load_default
+   File.open(File.join(Rails.root, "config", "settings.yaml"), 'r') do |f|
+    @@defaults = YAML::load(f)
+   end
+  end
+  load_default
 end
