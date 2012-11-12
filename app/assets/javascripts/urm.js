630b2e4ba5fbf3fc175d6508160ac773d088e7b8
afa <afa@afalone.local>
2012-11-12 16:06:16 +0400

    fix

--- app/assets/javascripts/urm.js
+++ app/assets/javascripts/urm.js
@@ -81,6 +81,17 @@ function fix_visibility(){
   $("tr:visible." + i).not(":first").children(".dms.js").hide();
  }
 }
+
+function dms_item_hide(obj){
+ //$("tr.item_" + obj + " td.icons div.icon div.dms").removeClass('active');
+ $("tr.dms_item_" + obj).hide();
+ $("tr.dms_item_" + obj).addClass("hidden");
+ if($("tr.analog_item_" + obj).add("tr.info_item_" + obj).not(".hidden").length == 0){
+  $("tr.gap_" + obj).hide();
+  $("tr.gap_" + obj).addClass("hidden");
+ }
+}
+
 function load_dms_bundle(from_where, need_load){
  if(need_load){
   $.getJSON(from_where, "", function(data){
@@ -90,13 +101,6 @@ function load_dms_bundle(from_where, need_load){
     $("tr.item_" + kk + " .icon .dms").addClass("active");
     $("tr.dms_item_" + kk + " th .plus").click(function(){
      var obj = $(this).parents("tr").find("th .icon input.after").first().val();
-     $("tr.item_" + obj + " td.icons div.icon div.dms").removeClass('active');
-     $("tr.dms_item_" + obj).hide();
-     $("tr.dms_item_" + obj).addClass("hidden");
-     if($("tr.analog_item_" + obj).add("tr.info_item_" + obj).not(".hidden").length == 0){
-      $("tr.gap_" + obj).hide();
-      $("tr.gap_" + obj).addClass("hidden");
-     }
      return false;
     });
    }
