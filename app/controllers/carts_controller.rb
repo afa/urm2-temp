90393321f5dd20b8c63bd11f17fc428001cce6af
afa <afa@afalone.local>
2012-11-21 11:36:02 +0400

    fix

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -149,9 +149,9 @@ class CartsController < ApplicationController
     #gon.changes = @changed
     @deliveries = User.current.deliveries
     gon.stores = @stores
+    @avail_sales = [""] + Axapta.sales_info_paged(1, :status_filter => 'backorder', :records_per_page => 64000).items.map{|s| [s.sales_id, s.sales_id] }
     gon.order = render_to_string :partial => "main/order_edit"
    end
-   @avail_sales = [""] + Axapta.sales_info_paged(1, :status_filter => 'backorder', :records_per_page => 64000).items.map{|s| [s.sales_id, s.sales_id] }
 
    respond_with do |format|
     format.js { render :layout => false }
