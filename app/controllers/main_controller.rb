f61e75cf85dc36fc53955cfb670c7e4ea1ce1eb5
afa <afa@afalone.local>
2012-11-28 16:35:56 +0400

    fix

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -56,7 +56,8 @@ class MainController < ApplicationController
    @stores = @carts.map(&:location_link).uniq.compact
    respond_with do |format|
     format.json do
-     render :json => {:dms => render_to_string( :partial => "main/dms_block.html", :locals => {:items => @items, :after => @after} ), :gap => render_to_string( :partial => "main/gap_line.html", :locals => {:after => @after}), :empty => render_to_string(:partial => "main/dms_empty.html", :locals => {:after => @after}), :cart => render_to_string(:partial => "carts/cart_table", :locals => {:cart => @carts, :app_list => @app_list})}
+     render :json => {:dms => render_to_string( :partial => "main/dms_block.html", :locals => {:items => @items, :after => @after} ), :gap => render_to_string( :partial => "main/gap_line.html", :locals => {:after => @after}), :empty => render_to_string(:partial => "main/dms_empty.html", :locals => {:after => @after})}
+     #render :json => {:dms => render_to_string( :partial => "main/dms_block.html", :locals => {:items => @items, :after => @after} ), :gap => render_to_string( :partial => "main/gap_line.html", :locals => {:after => @after}), :empty => render_to_string(:partial => "main/dms_empty.html", :locals => {:after => @after}), :cart => render_to_string(:partial => "carts/cart_table", :locals => {:cart => @carts, :app_list => @app_list})}
     end
     #format.js { render :layout => false }
     format.html do
