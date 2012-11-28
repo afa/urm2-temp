45c93c4ebfe5d56d0bcb8e669ad3e654c40c07cb
afa <afa@afalone.local>
2012-11-28 17:34:48 +0400

    fix

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -57,8 +57,13 @@ class MainController < ApplicationController
    respond_with do |format|
     format.json do
      #render :json => {:dms => render_to_string( :partial => "main/dms_block.html", :locals => {:items => @items, :after => @after} ), :gap => render_to_string( :partial => "main/gap_line.html", :locals => {:after => @after}), :empty => render_to_string(:partial => "main/dms_empty.html", :locals => {:after => @after})}
+     begin
      crt = render_to_string(:partial => "carts/cart_collection", :locals => {:cart => @carts, :app_list => @app_list})
+     rescue Exception => e
+     p "---mce", e, e.backtrace
      p "---mc-crt", crt
+     raise
+     end
      render :json => {:dms => render_to_string( :partial => "main/dms_block.html", :locals => {:items => @items, :after => @after} ), :gap => render_to_string( :partial => "main/gap_line.html", :locals => {:after => @after}), :empty => render_to_string(:partial => "main/dms_empty.html", :locals => {:after => @after}), :cart => crt}
     end
     #format.js { render :layout => false }
