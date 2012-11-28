3c636eaece554b9550bcc553e45d7b24ae5023c3
afa <afa@afalone.local>
2012-11-28 15:32:09 +0400

    fix

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -55,11 +55,12 @@ class MainController < ApplicationController
     @carts.select{|c| c.is_a?(CartWorld) }.each{|c| c.location_link = User.current.current_account.invent_location_id }
    end
    @app_list = Axapta.application_area_list || []
+   @stores = @carts.map(&:location_link).uniq.compact
    respond_with do |format|
     format.json do
      render :json => {:dms => render_to_string( :partial => "main/dms_block.html", :locals => {:items => @items, :after => @after} ), :gap => render_to_string( :partial => "main/gap_line.html", :locals => {:after => @after}), :empty => render_to_string(:partial => "main/dms_empty.html", :locals => {:after => @after}), :cart => render_to_string(:partial => "carts/cart_table", :locals => {:cart => @carts, :app_list => @app_list})}
     end
-    format.js { render :layout => false }
+    #format.js { render :layout => false }
     format.html do
      redirect_to root_path
     end
