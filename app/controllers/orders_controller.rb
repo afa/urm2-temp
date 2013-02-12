028877c78812b4a43915911a4697ee80b376fc5c
afa <afa@afalone.local>
2013-02-12 15:03:48 +0400

    fix

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -70,7 +70,7 @@ class OrdersController < ApplicationController
    @close_reasons = Axapta.sales_close_reason_list
    @order_info = Axapta.sales_info(:sales_id => params[:id], :show_external_invoice_num => true, :show_max_quotation_prognosis => true).first
    if @order_info.nil?
-    render :status => 404
+    render :status => 404, :text => 'notfound'
     return
    end
    @lines = Axapta.sales_lines_paged(@page, :sales_id => params[:id], :show_reserve_qty => true, :show_status_qty => true, :only_open => true)
