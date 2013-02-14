9e586ae9f7f96476418fae15bbb288c48cce7877
afa <afa@afalone.local>
2013-02-14 14:40:16 +0400

    fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -120,7 +120,7 @@ class UsersController < ApplicationController
    @filter.date_to = Date.current.strftime("%Y-%m-%d") if @filter.date_to.blank?
    @filter.date_from = 1.month.ago.strftime("%Y-%m-%d") if @filter.date_from.blank?
    @filter_hash.merge!(:date_to => @filter.date_to, :date_from => @filter.date_from)
-   unless @filter.format
+   unless params[:format]
     redirect_to sold_orders_users_path, :flash => {:error => "не задан формат"}
     return
    end
