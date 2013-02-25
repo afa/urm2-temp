ce4a64ae763d106fbcbd174be402a582622df2f0
afa <afa@afalone.local>
2013-02-25 13:23:37 +0400

    fix

---
+++ app/assets/javascripts/carts.js
@@ -0,0 +1,61 @@
+function cartsAddElementToCart(){
+ makeAjaxPost("/carts.json", 
+  [],
+  function(data, reply, xhr){
+   if(data.carts.length == 0){
+    $('.cart-table').add('#order').hide();
+    $("#cart_store table tr:has(td)").remove();
+    $.each(data.changes, function(idx, item){
+     $('table.search-products tr input.item-cart[value="' + item[0] + '"]').val(item[1]);
+     $('table.search-products tr input.dms-cart[value="' + item[0] + '"]').val(item[1]);
+    });
+    if (data.carts.length > 0){ //empty?
+     $("#cart_store table").append(data.rendered);
+     $.each(data.carts, function(idx, item){
+      //$("#cart_store table").append(item.line);
+      $('table.search-products input.item-cart[value="' + item.obj_id +'"]').parents("tr").find('td.input-in-cart input[type="text"]').val(item.amount);
+      $('table.search-products input.dms-cart[value="' + item.obj_id +'"]').parents("tr").find('td.input-in-cart input[type="text"]').val(item.amount);
+     });
+     $(".cart-table").add("#order").show();
+    }
+   }
+   if($("#cart_store table tr").length >= 1){
+    $("#cart_store").show();
+    $("#allow-order").show();
+   } else {
+    $("#cart_store").hide();
+    $("#allow-order").hide();
+   }
+   //! $("div#order").hide();
+   //$("div#order").children().remove();
+   //$("div#order").append(gon.order);
+   //? need form processing? :TODO:
+   $('.select').selectList();
+   $('.button-style').button();
+   $('.switch').switchControl();
+   activateCommit();
+   //$('.commit a.button-style').off("click");
+   //$('.commit a.button-style').on("click", function(){
+   // $(this).parents('form').submit();
+   // return false;
+   //});
+   if($(".calendar-input").length > 0){
+    $(".calendar-input").datepicker({ dateFormat: 'yy-mm-dd' });
+   }
+   activateSearchAllowOrderButton();
+   //! $("#order").hide();
+   //! $("#cancel_order").hide();
+   //activateSearchCancelButton();
+   //handleCartDelete();
+   apply_hover_in_table_on_mmove();
+   $('.form-hide .item').dropDown();
+   cartsHandleRadioPicks();
+   cartsHandleSaveOnFocusLost();
+   cartsProcessRadioPicks();
+   event.preventDefault();
+  },
+  function(){}
+ );
+}
+
+
