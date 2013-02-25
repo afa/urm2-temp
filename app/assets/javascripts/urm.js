9a16ab4be127bc589f39a9eeae3661911db88796
afa <afa@afalone.local>
2013-02-25 19:22:52 +0400

    fix

--- app/assets/javascripts/urm.js
+++ app/assets/javascripts/urm.js
@@ -34,7 +34,6 @@ function makeAjaxDestroy(ajaxUrl, functionSuccess, functionFailure){
 }
 
 function makeAjaxPost(ajaxUrl, dataHash, functionSuccess, functionFailure){
- alert(dataHash.first());
  $.ajax({
   type: "POST",
   url: ajaxUrl,
