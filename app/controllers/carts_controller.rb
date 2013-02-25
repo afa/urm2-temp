607bec8657e8d1f24ead4b85adf29737bd6fb041
afa <afa@afalone.local>
2013-02-25 17:34:31 +0400

    fix

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -11,6 +11,7 @@ class CartsController < ApplicationController
 
   def create
    @changed = []
+   p "---cacrpar", params
    #if params[:items] and !params[:items].blank?
     (params[:items] || {}).reject{|k, v| k == :commit }.reject{|k, v| v[:amount].blank? }.each do |k, v|
      @changed << [v[:cart], CartStore.copy_on_write(v)]
