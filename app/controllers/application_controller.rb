dc661c8455bb273eee29efc98d5ca59997a2444c
afa <afa@afalone.local>
2013-03-27 15:19:09 +0400

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
    Rails.logger.info "resc auth #{e}"
    rescue_action_without_handler(e)
   end
-  rescue_from StandartError do |e|
+  rescue_from StandardError do |e|
    Rails.logger.info "---+rescue: need redirect, #{e}, #{e.backtrace.first(3)}"
   # if self.class.class_variable_defined?(:@@auth_redirect_on_failed) && self.class.auth_redirect_on_failed
   #  redirect_to self.class.auth_redirect_on_failed
