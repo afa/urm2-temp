839e577878bf706ed13c8baf34131c68d665183c
afa <afa@afalone.local>
2013-03-27 15:06:56 +0400

    fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,11 @@ class ApplicationController < ActionController::Base
   helper ApplicationHelper
   include Afauth::Controller::App
   rescue_from Afauth::AuthError do |e|
+   Rails.logger.info "resc auth #{e}"
    rescue_action_without_handler(e)
   end
   rescue_from Exception do |e|
-   Rails.logger.info "---rescue: need redirect, #{e}, #{e.backtrace.first(3)}"
+   Rails.logger.info "---+rescue: need redirect, #{e}, #{e.backtrace.first(3)}"
   # if self.class.class_variable_defined?(:@@auth_redirect_on_failed) && self.class.auth_redirect_on_failed
   #  redirect_to self.class.auth_redirect_on_failed
   # elsif self.class.class_variable_defined?(:@@auth_redirect_on_failed_cb) && self.class.auth_redirect_on_failed_cb
