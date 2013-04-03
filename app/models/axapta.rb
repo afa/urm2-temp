e4dc1757b899c4a8275b6b5c37cc16656d3aa838
afa <afa@afalone.local>
2013-04-03 11:16:15 +0400

    fix

--- app/models/axapta.rb
+++ app/models/axapta.rb
@@ -150,11 +150,11 @@ class Axapta
    srch = args.first[:item_id] unless srch
    res = asks(:retail_price, "prices", args.as_hash.merge(:user_hash => axapta_hash))
    a = {}
-   locs = res.sort_by{|l| l["min_qty"] }[0, 4]
-   a.merge!("price1" => locs[0]["price"], "count1" => locs[0]["min_qty"]) if locs[0]
-   a.merge!("price2" => locs[1]["price"], "count2" => locs[1]["min_qty"]) if locs[1]
-   a.merge!("price3" => locs[2]["price"], "count3" => locs[2]["min_qty"]) if locs[2]
-   a.merge!("price4" => locs[3]["price"], "count4" => locs[3]["min_qty"]) if locs[3]
+   locs = res.sort_by{|l| l.min_qty }[0, 4]
+   a.merge!("price1" => locs[0].price, "count1" => locs[0].min_qty) if locs[0]
+   a.merge!("price2" => locs[1].price, "count2" => locs[1].min_qty) if locs[1]
+   a.merge!("price3" => locs[2].price, "count3" => locs[2].min_qty) if locs[2]
+   a.merge!("price4" => locs[3].price, "count4" => locs[3].min_qty) if locs[3]
    a
   end
 
