bdfc9ab374c519e7e0466e2a5a81d9d35d71c51f
afa <afa@afalone.local>
2013-04-23 15:26:23 +0400

    fix

--- app/models/offer/ask_man.rb
+++ app/models/offer/ask_man.rb
@@ -2,7 +2,7 @@
 class Offer::AskMan < Offer::Base
   def self.search(hsh)
    return AxaptaResults.new([], {:type => AxaptaState::WARN, :error => "not found", :message => I18n.t("errors.search.empty")}) if hsh.blank? || ((hsh[:query_string].blank? || hsh[:query_string].size < 3))
-   data = AxaptaResults.new([OpenStruct.new({:name => hsh[:query_string]})], {:type => AxaptaState::OK, :error => nil, :message => nil}).process{|d| fabricate(d) }
+   data = AxaptaResults.new([{:name => hsh[:query_string]}], {:type => AxaptaState::OK, :error => nil, :message => nil}).process{|d| fabricate(d) }
 
 # :show_delivery_prognosis => true,
    #TODO: to offers
