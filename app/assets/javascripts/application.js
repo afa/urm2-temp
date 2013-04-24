a9330c2d6dadd54ca680e0d6db864c780d184405
afa <afa@afalone.local>
2013-04-24 12:37:06 +0400

    fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,9 +26,4 @@
 //= require redactor
 //= require redactor_ru
 //= require redactor_toolbar_default
-//= require underscore
-//= require backbone
-//= require backbone_rails_sync
-//= require backbone_datalink
-//= require backbone/urm
 //= require_tree ./admin
