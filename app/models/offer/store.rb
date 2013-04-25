56dbf4fe1db6dfdd2162b3f21f2c9bf709a00bb2
afa <afa@afalone.local>
2013-04-25 14:50:31 +0400

    fix

--- app/models/offer/store.rb
+++ app/models/offer/store.rb
@@ -1,7 +1,7 @@
 class Offer::Store < Offer::Base
 
  @signature_fields = @base_signature_fields + [:location_id]
- attr_accessor :location_id, :brend_name, :brend_url, :qtys, :prices, :counts, :need_more, :qty_in_pack, :segment_rus, :body_name, :analog_exists, :forecast_available, :min_qty, :max_qty, :raw_location, :vend_proposal_date, :alt_prices, :application_area_mandatory, :dlv_prognoses, :pick_only
+ attr_accessor :location_id, :brend_name, :brend_url, :qtys, :prices, :counts, :need_more, :qty_in_pack, :segment_rus, :body_name, :analog_exists, :forecast_available, :min_qty, :max_qty, :raw_location, :vend_proposal_date, :alt_prices, :application_area_mandatory, :dlv_prognoses, :pick_only, :mpq
 
   def self.search(hsh)
    return AxaptaResults.new([], {:type => AxaptaState::WARN, :error => "not found", :message => I18n.t("errors.search.empty")}) if hsh.blank? || (hsh[:external_code].blank? && (hsh[:query_string].blank? || hsh[:query_string].size < 3))
@@ -18,6 +18,7 @@ class Offer::Store < Offer::Base
     hsh.locations.each do |loc|
      rez << self.new do |n|
       n.name = hsh.item_name
+      n.mpq = hsh.mpq
       n.brend = hsh.item_brend
       n.brend_name = hsh.item_brend_name
       n.brend_url = hsh.item_brend_url
